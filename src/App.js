cd891f97d9fbd62192541dcc9b82119ff0a9ea5b
Sahil <sahilv04@gmail.com>
2024-01-06 18:07:41 +0000

    basic

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { client } from './client';
+import { useReactToPrint } from 'react-to-print';
 
 const App = () => {
   const [profile, setProfile] = useState(null)
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
   useEffect(() => {
     client.getEntries({ content_type: "resume" }).then((res) => {
       setProfile(res.items[0].fields)
     })
   }, [])
+
   return (
     <div className="App">
-
+      <button onClick={handlePrint}>print</button>
+      <iframe id="ifmcontentstoprint" style={{ height: 0, width: 0, position: "absolute" }} />
       {
         profile &&
-        <div>
-          <div>{profile.name}</div>
-          <div>{profile.email}</div>
-          <div>{profile.phone}</div>
-          <div>{profile.location}</div>
+        <div className='resume-container'>
+          <div className='resume-content' ref={componentRef} >
+            <div className='resume-content-inner'>
+              <div className='primary-section'>
+
+                <div>{profile.name}</div>
+                <div>{profile.email}</div>
+                <div>{profile.phone}</div>
+                <div>{profile.location}</div>
+              </div>
+              <div className='secondary-section'>
+                <h2 className='heading section-heading'>Contact</h2>
+                Contact
+              </div>
+            </div>
+          </div>
         </div>
       }
+
     </div>
   );
 }

cc3c9ad0aa1be5e72e318708c67b9b948d3d4d81
Sahil <sahilv04@gmail.com>
2024-01-06 19:08:42 +0000

    title

---
+++ src/components/Experience/index.jsx
@@ -0,0 +1,11 @@
+import BlockHeading from "../BlockHeading"
+
+const Experience = ({ profile }) => {
+    return (
+        <div className="experience-container block-container">
+            <BlockHeading>Work Experience</BlockHeading>
+        </div>
+    )
+}
+
+export default Experience
--- src/components/PrimarySection/index.jsx
+++ src/components/PrimarySection/index.jsx
@@ -1,9 +1,11 @@
+import Experience from "../Experience"
 import ResumeTitle from "../ResumeTitle"
 
 const PrimarySection = ({ profile }) => {
     return (
         <div className='primary-section'>
             <ResumeTitle profile={profile} />
+            <Experience profile={profile} />
         </div>
     )
 }
